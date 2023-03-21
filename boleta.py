7a22c9cd2fbe67d338e0cd669412a6ce6dd56d72
alexis7893 <125474680+alexis7893@users.noreply.github.com>
2023-03-21 08:29:50 -0500

    Add files via upload

---
+++ boleta.py
@@ -0,0 +1,24 @@
+f = open ('boleta.text','a')
+f.write("|**************************boleta electronica********************\n")
+f.write("|                                  **plazabea\n")
+f.write("|                                                             RUC:49871364\n")
+f.write("|                                                       NUMERO DE BOLETA:000846\n")
+f.write("|TIENDA = plazabea\n"+"\n|NOMBRE = manuel alexis")
+f.write("-------------------------APELLIDOS = hinojosa yupanqui\n")
+f.write("|DNI = 998765432\n") 
+f.write("|DIRECCION = av 8 de octubre san juan de lurigancho\n")
+f.write("|TELEFONO = 933659403\n")
+f.write("|FECHA = 18/03/2023\n")
+f.write("|***************************************************************\n")
+f.write("|DESCRIPCION:| --------|CANTIDAD:|----------|PRECIO:|------|SUBTOTAL:|\n")
+f.write("conjunto de cocina----------------1-------------------|S/500|--------|S/500|\n")
+f.write("mueble ------------------------3-------------------|S/1200|--------|S/1200|\n")
+f.write("computadora------------1-------------------|S/2100--------|S/2100|\n")
+f.write("----------------------------------------------------TOTAL = |S/3200|\n")
+
+
+
+
+
+
+f.close()
